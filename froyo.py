45d44a3f677e555a8296c056dd50c3d925dff6ec
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-08-31 11:01:22 +0800

    Added renaming feature based on previous image

--- froyo.py
+++ froyo.py
@@ -15,9 +15,11 @@ for images in os.listdir(input_path):
     hsize = int((float(output.size[1])*float(wpercent)))
     gbr = output.resize((basewidth,hsize), Image.ANTIALIAS)
 
+    nama_baru = images.split(".", 1)[0]
+
     background = Image.open("twibbon.png")
     background.paste(gbr, ((background.width - gbr.width) // 2, (background.height - gbr.height) // 2))
-    background.save(output_path + str(counter) + ".png",optimize=True,quality=95)
+    background.save(output_path + str(nama_baru) + ".png",optimize=True,quality=95)
 
     print(f"Gambar {counter} berhasil diproses")
     counter += 1
