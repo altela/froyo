4d3b7bf105b4a03152f17c8a3851e904f69850f8
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-09-08 10:59:29 +0800

    Added block of code to create a image without twibbon

--- froyo.py
+++ froyo.py
@@ -14,9 +14,10 @@ for images in os.listdir(input_path):
     wpercent = (basewidth/float(output.size[0]))
     hsize = int((float(output.size[1])*float(wpercent)))
     gbr = output.resize((basewidth,hsize), Image.ANTIALIAS)
+    nama_baru_no_twibbon = images.split(".", 1)[0]
+    gbr.save(output_path + str(nama_baru_no_twibbon) + " empty.png",optimize=True,quality=95)
 
     nama_baru = images.split(".", 1)[0]
-
     background = Image.open("twibbon.png")
     background.paste(gbr, ((background.width - gbr.width) // 2, (background.height - gbr.height) // 2))
     background.save(output_path + str(nama_baru) + ".png",optimize=True,quality=95)
