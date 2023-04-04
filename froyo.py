cd8d9e2ec6cae91f0c5dba259ca2d34d3dce2c91
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-04-04 10:40:48 +0800

    Changed PIL dependency

--- froyo.py
+++ froyo.py
@@ -9,7 +9,10 @@ counter = 1
 
 for images in os.listdir(input_path):
     if images == ".DS_Store":
-        os.remove(".DS_Store")
+        try:
+            os.remove(".DS_Store")
+        except:
+            pass
     else:
         check_extension = os.path.splitext(images)
         file_extension = check_extension[1]
@@ -26,12 +29,12 @@ for images in os.listdir(input_path):
         hsize = int((float(output.size[1])*float(wpercent)))
         gbr = output.resize((basewidth,hsize), Image.ANTIALIAS)
         nama_baru_no_twibbon = images.split(".", 1)[0]
-        gbr.save(output_path + str(nama_baru_no_twibbon) + " empty.png",optimize=True,quality=95)
+        gbr.save(output_path + str(nama_baru_no_twibbon) + " No Watermark.png",optimize=True,quality=95)
 
-        nama_baru = images.split(".", 1)[0]
-        background = Image.open("twibbon.png")
-        background.paste(gbr, ((background.width - gbr.width) // 2, (background.height - gbr.height) // 2))
-        background.save(output_path + str(nama_baru) + ".png",optimize=True,quality=95)
+        # nama_baru = images.split(".", 1)[0]
+        # background = Image.open("twibbon.png")
+        # background.paste(gbr, ((background.width - gbr.width) // 2, (background.height - gbr.height) // 2))
+        # background.save(output_path + str(nama_baru) + ".png",optimize=True,quality=95)
 
         print(f"Gambar {counter} berhasil diproses")
         counter += 1
